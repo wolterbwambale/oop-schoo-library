d901ad8b97d7e3fd65b426294ef4e5c3da82b349
wolterbwambale <benplaan1@gmail.com>
2023-08-01 11:22:19 +0300

    update person class

--- person.rb
+++ person.rb
@@ -1,31 +1,24 @@
 class Person
-    attr_reader :id, :name, :age
-  
-    def initialize(id, age, name = "Unknown", parent_permission = true)
-      @id = id
-      @name = name
-      @age = age
-      @parent_permission = parent_permission
-    end
-  
-    def name=(name)
-      @name = name
-    end
-  
-    def age=(age)
-      @age = age
-    end
-  
-    private
-  
-    def of_age?
-      @age >= 18
-    end
-  
-    public
-  
-    def can_use_services?
-      of_age? || @parent_permission
-    end
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def initialize(id, age, name = 'Unknown', parent_permission: true)
+    @id = id
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
   end
-  
+
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+
+  public
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+end
